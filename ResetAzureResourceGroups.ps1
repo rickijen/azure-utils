fc676143855cc273d79be6c6c2ba54374df15ad6
rickijen <rickijen@gmail.com>
2015-11-24 16:19:35 -0800

    instead of just delete resource groups without resource, delete all.

--- ResetAzureResourceGroups.ps1
+++ ResetAzureResourceGroups.ps1
@@ -10,7 +10,7 @@ if ($isOk.ToUpper().Equals("Y"))
     Switch-AzureMode -Name AzureResourceManager
 
     $resourceGroups = Get-AzureResourceGroup
-    $fullAuto = Read-Host 'Do you want to delete all the resource groups which do not have any resources. If you select No you can choose one by one and delete them [Y/N] ?'
+    $fullAuto = Read-Host 'Delete all resource groups? If you select No you can delete one by one [Y/N] ?'
 
     $found = 0;
     $deleted = 0;
@@ -18,26 +18,16 @@ if ($isOk.ToUpper().Equals("Y"))
     foreach($resourceGroup in $resourceGroups)
     {
         $rgName = $resourceGroup.ResourceGroupName
-        
-        $resources = Get-AzureResource -ResourceGroupName $rgName
-   
-        if ($resources.Count -eq 0)
-        {
-            $found++
-            Write-Host Resource Group " $rgName " does not contain any resources.
 
-            if($fullAuto.ToUpper().Equals("N"))
-            {
-                $input = Read-Host 'Do you want to delete it [Y/N] ?'
-                if ($input.ToUpper().Equals("Y"))
-                {
-                    Write-Host Deleting Resource Group - $rgName
-                    Remove-AzureResourceGroup -Name $resourceGroup.ResourceGroupName -Force -Verbose
-                    Write-Host Deleted Resource Group - $rgName
-                    $deleted++
-                }
-            }
-            else
+        $resources = Get-AzureResource -ResourceGroupName $rgName
+
+        $found++
+        Write-Host Resource Group " $rgName " does not contain any resources.
+
+        if($fullAuto.ToUpper().Equals("N"))
+        {
+            $input = Read-Host 'Do you want to delete it [Y/N] ?'
+            if ($input.ToUpper().Equals("Y"))
             {
                 Write-Host Deleting Resource Group - $rgName
                 Remove-AzureResourceGroup -Name $resourceGroup.ResourceGroupName -Force -Verbose
@@ -45,13 +35,18 @@ if ($isOk.ToUpper().Equals("Y"))
                 $deleted++
             }
         }
+        else
+        {
+            Write-Host Deleting Resource Group - $rgName
+            Remove-AzureResourceGroup -Name $resourceGroup.ResourceGroupName -Force -Verbose
+            Write-Host Deleted Resource Group - $rgName
+            $deleted++
+        }
     }
 
-    Write-Host $deleted / $found Resource Groups deleted with no resources
+    Write-Host $deleted / $found Resource Groups deleted
 
     Switch-AzureMode -Name AzureServiceManagement
 }
-else
-{
-    exit
-}
+
+exit
