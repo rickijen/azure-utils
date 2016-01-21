ec49b0db7950005cb5431a3069cb9cf8f0a8bf57
rickijen <rickijen@gmail.com>
2016-01-20 16:09:49 -0800

    added startup dir

---
+++ startup/addazureaccount.ps1
@@ -0,0 +1,29 @@
+﻿#
+# Set Credentials for Azure login
+#
+
+# Generate password file
+#$credential = Get-Credential
+#$credential.Password | ConvertFrom-SecureString | Set-Content c:\users\bach\azpassword2.txt
+
+$installedPasswordFile = "c:\azpassword.txt"
+$packagedPasswordFile = Join-Path (Split-Path $profile) azpassword.txt
+
+If (Test-Path $installedPasswordFile){
+    # // File exists
+    $passwordFile = $installedPasswordFile
+} Else {
+    Write-Output "Use packaged password"
+    $passwordFile = $packagedPasswordFile
+}
+
+$azUser = "coadmin@rickijengmail.onmicrosoft.com"
+$azPassword = Get-Content $passwordFile | ConvertTo-SecureString
+$azCredential = New-Object System.Management.Automation.PSCredential $azUser,$azPassword
+
+Write-Output "Add ARM account"
+Add-AzureRmAccount -Credential $azCredential
+
+Write-Output "Add ASM account"
+Add-AzureAccount -Credential $azCredential
+
