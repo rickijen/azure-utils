27bd7e48cc33d419bc68df1801b38fb5b8e0fb5c
rickijen <rickijen@gmail.com>
2016-01-24 00:07:36 -0800

    need to test further.

--- startup/NewWindowsVM-ARM.ps1
+++ startup/NewWindowsVM-ARM.ps1
@@ -87,3 +87,59 @@ $vm=Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $vhdURI -CreateOption fr
 
 # Go!
 New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $vm
+
+
+#
+#
+#
+
+<#
+
+# Set values for existing resource group and storage account names
+$rgName="LOBServers"
+$locName="West US"
+$saName="contosolobserverssa"
+
+# Set the existing virtual network and subnet index
+$vnetName="AZDatacenter"
+$subnetIndex=0
+$vnet=Get-AzureRMVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
+
+# Create the NIC
+$nicName="LOB07-NIC"
+$domName="contoso-vm-lob07"
+$pip=New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $rgName -DomainNameLabel $domName -Location $locName -AllocationMethod Dynamic
+$nic=New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[$subnetIndex].Id -PublicIpAddressId $pip.Id
+
+# Specify the name, size, and existing availability set
+$vmName="LOB07"
+$vmSize="Standard_A3"
+$avName="WEB_AS"
+$avSet=Get-AzureRmAvailabilitySet –Name $avName –ResourceGroupName $rgName
+$vm=New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize -AvailabilitySetId $avset.Id
+
+# Add a 200 GB additional data disk
+$diskSize=200
+$diskLabel="APPStorage"
+$diskName="21050529-DISK02"
+$storageAcc=Get-AzureRmStorageAccount -ResourceGroupName $rgName -Name $saName
+$vhdURI=$storageAcc.PrimaryEndpoints.Blob.ToString() + "vhds/" + $vmName + $diskName  + ".vhd"
+Add-AzureRmVMDataDisk -VM $vm -Name $diskLabel -DiskSizeInGB $diskSize -VhdUri $vhdURI -CreateOption empty
+
+# Specify the image and local administrator account, and then add the NIC
+$pubName="MicrosoftWindowsServer"
+$offerName="WindowsServer"
+$skuName="2012-R2-Datacenter"
+$cred=Get-Credential -Message "Type the name and password of the local administrator account."
+$vm=Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vm=Set-AzureRmVMSourceImage -VM $vm -PublisherName $pubName -Offer $offerName -Skus $skuName -Version "latest"
+$vm=Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+
+# Specify the OS disk name and create the VM
+$diskName="OSDisk"
+$storageAcc=Get-AzureRmStorageAccount -ResourceGroupName $rgName -Name $saName
+$osDiskUri=$storageAcc.PrimaryEndpoints.Blob.ToString() + "vhds/" + $vmName + $diskName  + ".vhd"
+$vm=Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $osDiskUri -CreateOption fromImage
+New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $vm
+
+#>
