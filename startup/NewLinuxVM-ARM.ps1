7d9912c9c4eaaf28081abfeff2301a52bb3aa394
rickijen <rickijen@gmail.com>
2016-01-23 23:59:38 -0800

    added new scripts based on ARM and ASM

---
+++ startup/NewLinuxVM-ARM.ps1
@@ -0,0 +1,79 @@
+﻿# names
+$machineName = ‘redondomc’
+
+# New resource group and resources required to build a new VM
+<#
+New-AzureRmResourceGroup -Name redondo2 -Location westus
+New-AzureRmNetworkSecurityGroup -Name redondo2-nsg -ResourceGroupName redondo2 -Location westus
+New-AzureRmStorageAccount -ResourceGroupName redondo2 -Type Standard_LRS -Location westus -Name redondostoragewest
+New-AzureRmAvailabilitySet -Name mc-availability-set -ResourceGroupName redondo2 -Location westus
+#>
+
+#Virtual Network with 2 subnets
+<#
+$resourceGroupName="redondo2"
+$location="westus"
+$virtualNetworkName = ‘redondo2-vnet’
+$frontendSubnet=New-AzureRmVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix 10.0.1.0/24
+$backendSubnet=New-AzureRmVirtualNetworkSubnetConfig -Name backendSubnet -AddressPrefix 10.0.2.0/24
+New-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix 10.0.0.0/16 -Subnet $frontendSubnet,$backendSubnet
+#>
+
+$resourceGroupName = ‘redondo2’
+$virtualNetworkName = ‘redondo2-vnet’
+$networkSecurityGroupName = ‘redondo2-nsg’
+$applicationServerAvailabilitySetName = ‘mc-availability-set’
+$storageAccountname = ‘redondostoragewest’
+
+# variables
+$location = ‘westus’
+$applicationServerSize = ‘Basic_A2’
+$imagePublisher = ‘openlogic’
+$imageOffer = ‘centos’
+$ubuntuOSSku = ‘6.7’
+$adminUsername = ‘weswes’
+$sshKeyPath = ‘/home/’ + $adminUsername + ‘/.ssh/authorized_keys’
+$sshKeyData = [IO.File]::ReadAllText("C:\Users\rijen\rijen_public_key")
+
+# existing resources
+$applicationServerSecurityGroup = Get-AzureRmNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName
+$applicationServerAvailabilitySet = Get-AzureRmAvailabilitySet –Name $applicationServerAvailabilitySetName –ResourceGroupName $resourceGroupName
+$virtualNetwork = $virtualNetwork = Get-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
+$storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountname
+
+# NIC & DNS name
+<#
+$nicName="mc-nic0"
+$domName="redondomc"
+$subnetIndex=0
+$pip = New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $rgName -DomainNameLabel $domName -Location $locName -AllocationMethod Dynamic
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[$subnetIndex].Id -PublicIpAddressId $pip.Id
+#>
+
+# create new resources
+$publicIpAddress = New-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName -AllocationMethod Dynamic -Name $machineName -Location $location -DomainNameLabel $machineName
+$networkInterface = New-AzureRmNetworkInterface -Name $machineName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[$subnetIndex].Id -PublicIpAddressId $publicIpAddress.Id
+$osDiskUri = $storageAccount.PrimaryEndpoints.Blob.ToString() + “vhds/” + $machineName + “.vhd”
+
+$sshPublicKey = [Microsoft.Azure.Management.Compute.Models.SshPublicKey]::new()
+$sshPublicKey.KeyData = $sshKeyData
+$sshPublicKey.Path = $sshKeyPath
+$sshPublicKeyList = New-Object ‘System.Collections.Generic.List[Microsoft.Azure.Management.Compute.Models.SshPublicKey]’
+$sshPublicKeyList.Add($sshPublicKey)
+
+$osProfile = [Microsoft.Azure.Management.Compute.Models.OsProfile]::new()
+$osProfile.ComputerName = $machineName
+$osProfile.AdminUsername = $adminUsername
+$osProfile.LinuxConfiguration = [Microsoft.Azure.Management.Compute.Models.LinuxConfiguration]::new()
+$osProfile.LinuxConfiguration.DisablePasswordAuthentication = $true
+$osProfile.LinuxConfiguration.Ssh = [Microsoft.Azure.Management.Compute.Models.SshConfiguration]::new()
+$osProfile.LinuxConfiguration.Ssh.PublicKeys = $sshPublicKeyList
+
+$virtualMachineConfig = New-AzureRmVMConfig -VMName $machineName -VMSize $applicationServerSize -AvailabilitySetId $applicationServerAvailabilitySet.Id
+$virtualMachineConfig = Set-AzureRmVMSourceImage -VM $virtualMachineConfig -PublisherName $imagePublisher -Offer $imageOffer -Skus $ubuntuOSSku -Version “latest”
+$virtualMachineConfig = Add-AzureRmVMNetworkInterface -VM $virtualMachineConfig -Id $networkInterface.Id
+$virtualMachineConfig = Set-AzureRmVMOSDisk -VM $virtualMachineConfig -Name $machineName -VhdUri $osDiskUri -CreateOption fromImage
+$virtualMachineConfig.OSProfile = $osProfile
+
+# Specify the OS disk name and create the VM
+New-AzureRmVM -ResourceGroupName $resourceGroupName -Location $location -VM $virtualMachineConfig
---
+++ startup/NewWindowsVM-ARM.ps1
@@ -0,0 +1,89 @@
+﻿#
+# New Centos VM in ARM
+#
+
+
+
+# New Storage account
+<#
+$rgName="redondo"
+$locName="South Central US"
+$saName="redondostorage"
+$saType="Standard_LRS"
+New-AzureRmStorageAccount -Name $saName -ResourceGroupName $rgName –Type $saType -Location $locName
+#>
+
+$domName="redondomc2"
+$loc="southcentralus"
+Test-AzureRmDnsAvailability -DomainQualifiedName $domName -Location $loc
+
+# New Availability set
+<#
+$avName="<availability set name>"
+$rgName="<resource group name>"
+$locName="<location name, such as West US>"
+New-AzureRmAvailabilitySet –Name $avName –ResourceGroupName $rgName -Location $locName
+#>
+
+# Create new Load Balancer set
+<#
+#>
+
+#Virtual Network with 2 subnets
+<#
+$rgName="redondo"
+$locName="SouthCentralUS"
+$frontendSubnet=New-AzureRmVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix 10.0.1.0/24
+$backendSubnet=New-AzureRmVirtualNetworkSubnetConfig -Name backendSubnet -AddressPrefix 10.0.2.0/24
+New-AzureRmVirtualNetwork -Name redondovnet -ResourceGroupName $rgName -Location $locName -AddressPrefix 10.0.0.0/16 -Subnet $frontendSubnet,$backendSubnet
+#>
+
+$rgName="redondo"
+$locName="southcentralus"
+$saName="redondostorage"
+$vnetName="redondovnet"
+$vnet=Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName | Select Subnets
+
+# NIC & DNS name
+$nicName="mc-nic0"
+$domName="redondomc"
+$subnetIndex=0
+$pip = New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $rgName -DomainNameLabel $domName -Location $locName -AllocationMethod Dynamic
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[$subnetIndex].Id -PublicIpAddressId $pip.Id
+
+# Get VM Size
+<#
+$locName="southcentralus"
+Get-AzureRmVMSize -Location $locName | Select Name
+#>
+
+$vmName="redondomc2"
+$vmSize="Basic_A2"
+$avName="mc-availability-set"
+$avSet=Get-AzureRmAvailabilitySet –Name $avName –ResourceGroupName $rgName
+
+#Start VM config
+$vm=New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize -AvailabilitySetId $avset.Id
+
+# Additional Disk
+$diskSize=1
+$diskLabel="mcDataDisk01"
+$diskName="20160123-DISK01"
+$storageAcc=Get-AzureRmStorageAccount -ResourceGroupName $rgName -Name $saName
+$vhdURI=$storageAcc.PrimaryEndpoints.Blob.ToString() + "vhds/" + $vmName + $diskName  + ".vhd"
+Add-AzureRmVMDataDisk -VM $vm -Name $diskLabel -DiskSizeInGB $diskSize -VhdUri $vhdURI  -CreateOption empty
+
+#$image=Get-AzureRmVMImage -Location "south central US" -Offer "centos" -PublisherName "openlogic" -Skus "6.7"
+
+$pubName="openlogic"
+$offerName="centos"
+$skuName="6.7"
+
+$cred=Get-Credential -Message "Type the name and password of the local administrator account."
+$vm=Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vm=Set-AzureRmVMSourceImage -VM $vm -PublisherName $pubName -Offer $offerName -Skus $skuName -Version "latest"
+$vm=Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+$vm=Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $vhdURI -CreateOption fromImage
+
+# Go!
+New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $vm
