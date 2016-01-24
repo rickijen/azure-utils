670eea682ef806e1dfa12eb4a601ef3564b1b8d3
rickijen <rickijen@gmail.com>
2016-01-23 19:11:34 -0800

    updated windows new vm script and added rdp file

--- startup/NewWindowsVM.ps1
+++ startup/NewWindowsVM.ps1
@@ -2,21 +2,6 @@
 # New Windows VM
 #
 
-$installedPasswordFile = "C:\Users\rijen\azure-utils\startup\azpassword.txt"
-$packagedPasswordFile = Join-Path (Split-Path $profile) azpassword.txt
-
-If (Test-Path $installedPasswordFile){
-    # // File exists
-    $passwordFile = $installedPasswordFile
-} Else {
-    Write-Output "Use packaged password"
-    $passwordFile = $packagedPasswordFile
-}
-
-$azUser = "coadmin@rickijengmail.onmicrosoft.com"
-$azPassword = Get-Content $passwordFile | ConvertTo-SecureString
-$azCredential = New-Object System.Management.Automation.PSCredential $azUser,$azPassword
-
 $family="Windows Server 2012 R2 Datacenter"
 $image=Get-AzureVMImage | where { $_.ImageFamily -eq $family } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
 $vmname="redondmcw"
@@ -24,15 +9,16 @@ $vmsize="Basic_A2"
 Set-AzureSubscription -SubscriptionId 7f3cc5e6-2fda-4486-b5c4-66de147a9f86 -CurrentStorageAccountName redondomc
 $availset=Get-AzureRmAvailabilitySet -ResourceGroupName redondo
 $vm1=New-AzureVMConfig -Name $vmname -InstanceSize $vmsize -ImageName $image -AvailabilitySetName $availset.Name
-#$cred=Get-Credential -Message "Type the name and password of the local administrator account."
-#$vm1 | Add-AzureProvisioningConfig -Windows -AdminUsername $cred.GetNetworkCredential().Username -Password $cred.GetNetworkCredential().Password
-$vm1 | Add-AzureProvisioningConfig -Windows -AdminUsername "weswes" -Password $azCredential.Password
+$cred=Get-Credential -Message "Type the name and password of the local administrator account."
+$vm1 | Add-AzureProvisioningConfig -Windows -AdminUsername $cred.GetNetworkCredential().Username -Password $cred.GetNetworkCredential().Password
+#$vm1 | Add-AzureProvisioningConfig -Windows -AdminUsername "weswes" -Password $azCredential.Password
 $vm1 | Set-AzureSubnet -SubnetNames "Subnet-1"
 $disksize=1
 $disklabel="DCData"
 $lun=0
 $hcaching="None"
 $vm1 | Add-AzureDataDisk -CreateNew -DiskSizeInGB $disksize -DiskLabel $disklabel -LUN $lun -HostCaching $hcaching
-$svcname="redondomcw"
+$svcname="redondomc"
+#New-AzureService -ServiceName $svcname -Location "South Central US"
 $vnetname="redondomc"
-New-AzureVM –ServiceName $svcname -VMs $vm1 -VNetName $vnetname
+New-AzureVM –ServiceName $svcname -VMs $vm1
