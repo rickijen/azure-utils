3bd2e5f319fa37faf48f1dfc7af9722177441cc9
rickijen <rickijen@gmail.com>
2016-01-24 22:22:38 -0800

    added quickstart templates homepage

--- startup/templatesDeployment.ps1
+++ startup/templatesDeployment.ps1
@@ -1,3 +1,7 @@
-﻿New-AzureRmResourceGroupDeployment -Name ubuntuwithoms -ResourceGroupName redondo3 -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-oms-extension-ubuntu-vm/azuredeploy.json
+﻿#
+# https://azure.microsoft.com/en-us/documentation/templates/
+#
+
+New-AzureRmResourceGroupDeployment -Name ubuntuwithoms -ResourceGroupName redondo3 -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-oms-extension-ubuntu-vm/azuredeploy.json
 
 New-AzureRmResourceGroupDeployment -Name minecraftonubuntu -ResourceGroupName redondo3 -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/minecraft-on-ubuntu/azuredeploy.json
