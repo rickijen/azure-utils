52e63d23ac05d8bfe51de2347ad6c1ad8f58b97b
Rick Jen <rijen@microsoft.com>
2016-07-07 17:43:30 -0700

    modified DeployPAN.ps1

--- startup/DeployPAN.ps1
+++ startup/DeployPAN.ps1
@@ -22,25 +22,17 @@ $Sku = ‘byol’
 $virtualNetwork = Get-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
 $storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountname
 
-# create new nics
+# create 1 new public IP & 3 new nics
 # 3 subnets for PAN have to be created prior to running the following and the Subnet ID has to match
+#
 $publicIpAddress = New-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName -AllocationMethod Dynamic -Name $machineName -Location $location -DomainNameLabel $machineName
-$networkInterface0 = New-AzureRmNetworkInterface -Name 'panNicMgmt2' -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[2].Id -PublicIpAddressId $publicIpAddress.Id
-$networkInterface1 = New-AzureRmNetworkInterface -Name 'panNicDmz2' -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[3].Id
-$networkInterface2 = New-AzureRmNetworkInterface -Name 'panNicTrust2' -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[4].Id
+$networkInterface0 = New-AzureRmNetworkInterface -Name 'pan002NicMgmt' -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[2].Id -PublicIpAddressId $publicIpAddress.Id
+$networkInterface1 = New-AzureRmNetworkInterface -Name 'pan002NicDmz' -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[3].Id
+$networkInterface2 = New-AzureRmNetworkInterface -Name 'pan002NicTrust' -ResourceGroupName $resourceGroupName -Location $location -SubnetId $virtualNetwork.Subnets[4].Id
 
 # Prepare OS Disk
 $diskName="OSDisk"
 $osDiskUri = $storageAccount.PrimaryEndpoints.Blob.ToString() + “vhds/” + $machineName + $diskName + “.vhd”
-
-# Create a new Linux osProfile object for configuration
-#$osProfile = [Microsoft.Azure.Management.Compute.Models.OsProfile]::new()
-#$osProfile.ComputerName = $machineName
-#$osProfile.AdminUsername = $adminUsername
-#$osProfile.AdminPassword = $adminPassword
-#$osProfile.LinuxConfiguration = [Microsoft.Azure.Management.Compute.Models.LinuxConfiguration]::new()
-#$osProfile.LinuxConfiguration.DisablePasswordAuthentication = $false
-#$osProfile.LinuxConfiguration.Ssh = [Microsoft.Azure.Management.Compute.Models.SshConfiguration]::new()
 
 # New VM config
 $virtualMachineConfig = New-AzureRmVMConfig -VMName $machineName -VMSize $vmSize
