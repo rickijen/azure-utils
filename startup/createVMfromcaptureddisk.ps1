758a2dc55c5812da78b667c4a9c1925756075fd3
Rick Jen <rijen@microsoft.com>
2016-10-03 17:33:05 -0700

    this script will create a new vm based on a vhd in storage account.

--- startup/createVMfromcaptureddisk.ps1
+++ startup/createVMfromcaptureddisk.ps1
@@ -1,43 +1,47 @@
-﻿$pipName = "newubuntupip"
-$rgName = "newrg"
-$location = "westus"
-$subnet1Name = "newubuntusubnet01"
-$vnetSubnetAddressPrefix = "10.2.0.0/24"
-$vnetName = "newvnet01"
-$vnetAddressPrefix = "10.2.0.0/16"
+﻿Login-AzureRmAccount
+Set-AzureRmContext -SubscriptionId 2e45430a-b715-4eb1-9bb0-3f9425ee2711
 
+$rgName = "ISCALA-PROD"
+$location = "West Europe"
+$storageAccName = "iscala"
+$vmName = "AZISCALAMOSAPT1"
+# https://azure.microsoft.com/documentation/articles/virtual-machines-windows-sizes/
+$vmSize = "Standard_F1s"
+# Name of the disk that holds the OS
+$osDiskName = "iScalaOSDisk"
+$urlOfUploadedImageVhd = "https://iscala.blob.core.windows.net/vhds/iscalaimage.vhd"
 
-$pip = New-AzureRmPublicIpAddress -Name $pipName -ResourceGroupName $rgName -Location $location -AllocationMethod Dynamic
-$nic = New-AzureRmNetworkInterface -Name $nicname -ResourceGroupName $rgName -Location $location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+# Get virtual network subnet settings
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $rgName
 
+# Create public IP and Nic
+$ipName = "iScalaPublicIp"
+$pip = New-AzureRmPublicIpAddress -Name $ipName -ResourceGroupName $rgName -Location $location -AllocationMethod Dynamic
 
-#Enter a new user name and password in the pop-up for the following
+$nicName = "iScalaNic"
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+
+# Enter a new user name and password to use as the local administrator account for the remotely accessing the VM
 $cred = Get-Credential
 
-#Get the storage account where the captured image is stored
-$storageAcc = Get-AzureRmStorageAccount -ResourceGroupName $rgName -AccountName newrg09376
+#Get the storage account where the uploaded image is stored
+$storageAcc = Get-AzureRmStorageAccount -ResourceGroupName $rgName -AccountName $storageAccName
 
 #Set the VM name and size
-$vmConfig = New-AzureRmVMConfig -Name newubuntu01 -VMSize "Standard_DS1_v2"
+#Use "Get-Help New-AzureRmVMConfig" to know the available options for -VMsize
+$vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
 
 #Set the Windows operating system configuration and add the NIC
-$vm = Set-AzureRmVMOperatingSystem -VM $vmConfig -Linux -ComputerName newubuntu01 -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vm = Set-AzureRmVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
 
 $vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
 
-$osDiskName = "newubuntuOsDisk"
-$vmName = "newubuntu01"
-
 #Create the OS disk URI
-$osDiskUri = '{0}vhds/{1}{2}.vhd' -f $storageAcc.PrimaryEndpoints.Blob.ToString(), $vmName.ToLower(), $osDiskName
+$osDiskUri = '{0}vhds/{1}-{2}.vhd' -f $storageAcc.PrimaryEndpoints.Blob.ToString(), $vmName.ToLower(), $osDiskName
 
-$urlOfCapturedImageVhd = "https://newrg09376.blob.core.windows.net/vhds/generalizedubuntu-osDisk.401d8e43-ec18-4da7-825e-66a6147a97a5.vhd"
-
-#Configure the OS disk to be created from image (-CreateOption fromImage) and give the URL of the captured image VHD for the -SourceImageUri parameter.
-#We found this URL in the local JSON template in the previous sections.
-$vm = Set-AzureRmVMOSDisk -VM $vm -Name $osDiskName -VhdUri $osDiskUri -CreateOption fromImage -SourceImageUri $urlOfCapturedImageVhd -Linux
+#Configure the OS disk to be created from the image (-CreateOption fromImage), and give the URL of the uploaded image VHD for the -SourceImageUri parameter
+#You set this variable when you uploaded the VHD
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name $osDiskName -VhdUri $osDiskUri -CreateOption fromImage -SourceImageUri $urlOfUploadedImageVhd -Windows
 
 #Create the new VM
-
-
-
+New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vm
